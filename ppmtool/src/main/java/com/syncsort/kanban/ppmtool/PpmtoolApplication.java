c677febad16923db61eb8c776a8ed9c02c0e8df3
“Susmit07” <susmitsircar@gmail.com>
2019-08-31 11:55:11 +0530

    Initial Commit

---
+++ ppmtool/src/main/java/com/syncsort/kanban/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.syncsort.kanban.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
