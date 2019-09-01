fcb173f297527b28feccf8e45e433e8f2b4b6179
“Susmit07” <susmitsircar@gmail.com>
2019-09-01 16:38:30 +0530

    Adding Binding result object for proper error response

--- ppmtool/src/main/java/com/syncsort/kanban/ppmtool/controller/ProjectController.java
+++ ppmtool/src/main/java/com/syncsort/kanban/ppmtool/controller/ProjectController.java
@@ -5,10 +5,17 @@ import com.syncsort.kanban.ppmtool.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,8 +24,16 @@ public class ProjectController {
     @Autowired
     ProjectService projectService;
 
+    // @Valid for better readable exception is the response due to javax validation.
+    // Binding result is an interface that invokes the validator on the object and binds the result to it
     @PostMapping
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            Map<String, String> validationErrors = bindingResult.getFieldErrors()
+                    .stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+            return new ResponseEntity< Map<String, String>>(validationErrors, HttpStatus.BAD_REQUEST);
+        }
         projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
