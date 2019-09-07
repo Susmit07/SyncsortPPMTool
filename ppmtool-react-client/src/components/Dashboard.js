b523283ffeda1fc72595694d8541181db459887a
“Susmit07” <susmitsircar@gmail.com>
2019-09-08 01:16:45 +0530

    React UI Client First commit with Dashboard component.

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+// get the React object from the react module
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+  render() {
+    return <h1>Welcome to the Dashboard</h1>;
+  }
+}
+
+//expose the HelloWorld component to other modules
+export default Dashboard;
